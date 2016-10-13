2eccc83f81401dab7aef6e9714586dbd92aef1f7
Mallika Mudda <muddamallika149189@gmail.com>
2016-10-13 16:10:51 +0200

    trial code run

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,36 @@ router.get('/', function(req, res, next) {
 
 /*Health check api*/
 router.get('/healthcheck', function(req, res) {
-  var responseString = { message: 'OK' };
-  res.send(responseString);
+  var responseObject = { message: 'OK' };
+  res.send(responseObject);
 });
 
+var database = [];
+
+router.post('/ilike/:icecreamchoice/:name', function(req, res){
+  if(req.body.formfactor) {
+    console.log(req.body.formfactor);
+  }else {
+    console.log('No form factor');
+  }
+  var choice = req.params.icecreamchoice;
+  var name = req.params.name;
+  if(name == 'mallika'){
+    database.push({choice: choice, name: name});
+    var responseObject = {message: 'Hey ' + name + ' .I like '+ choice + 'too!'};
+    res.send(responseObject);
+  }
+  else{
+    res.status(400).send();
+  }
+});
+
+router.get('/likes', function(req, res){
+  if(database.length == 0) {
+    res.status(404).send();
+  } else {
+    res.send(database);
+  }
+
+});
 module.exports = router;
